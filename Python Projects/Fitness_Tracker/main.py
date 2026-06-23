32bcba7565551fea89c7818625bc95bf73bbbda1
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-06-23 15:55:21 -0230

    testing new features

--- Python Projects/Fitness_Tracker/main.py
+++ Python Projects/Fitness_Tracker/main.py
@@ -3,7 +3,10 @@
 from models.user_profile import UserProfile
 from models.workout import Workout
 from models.goal import Goal
+
 from services.fitness_tracker import FitnessTracker
+from services.calories_calculator import calculate_calories
+from services.goal_tracker import GoalTracker
 
 def main():
     fitness_tracker = FitnessTracker()
@@ -11,43 +14,69 @@ def main():
     profile = UserProfile("Stephen", 180, "lbs")
     fitness_tracker.set_profile(profile)
 
+    # Test workout and calories 1
     workout1 = Workout(
         "Running",
         30,
         "medium",
-        250,
+        calories1,
         "Easy test run"
     )
 
-    workout2 = Workout(
-        "Weights",
-        45,
-        "high",
-        400,
-        "Upper body workout"
+    calories1 = calculate_calories(
+        "Running",
+        "medium",
+        profile.weight,
+        30,
+        profile.weight_unit
     )
 
     fitness_tracker.add_workout(workout1)
+
+    # Test workout and calories 2
+    workout2 = Workout(
+        "Cycling",
+        45,
+        "high",
+        calories2,
+        "Intense cycling session"
+    )
+
+    calories2 = calculate_calories(
+        "Cycling",
+        "high",
+        profile.weight,
+        45,
+        profile.weight_unit
+    )
+
     fitness_tracker.add_workout(workout2)
 
+    # Test goal and goal tracking
     goal = Goal(
         4,
-        180,
+        120,
         1500,
         "Running"
     )
 
     fitness_tracker.add_goal(goal)
 
-    print("\n=== Profile ===")
+    # Print out results
+    print("\n=== User Profile ===")
     fitness_tracker.view_profile()
 
     print("\n=== Workouts ===")
     fitness_tracker.view_workouts()
 
+    print("\n=== Workout Summary ===")
+    fitness_tracker.workout_summary()
+
     print("\n=== Goals ===")
     fitness_tracker.view_goals()
 
+    print("\n=== Goal Progress ===")
+    fitness_tracker.view_goal_progress()
 
 if __name__ == "__main__":
     main()
