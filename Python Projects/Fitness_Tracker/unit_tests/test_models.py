5e9cc2d8b9afa82a71b10c1bfb73f0f517d9348c
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-06-23 17:42:10 -0230

    finished basic models test

--- Python Projects/Fitness_Tracker/unit_tests/test_models.py
+++ Python Projects/Fitness_Tracker/unit_tests/test_models.py
@@ -6,6 +6,8 @@ import unittest
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from models.user_profile import UserProfile
+from models.workout import Workout
+from models.goal import Goal
 
 # User Profile
 class TestUserProfile(unittest.TestCase):
@@ -18,10 +20,27 @@ class TestUserProfile(unittest.TestCase):
         self.assertEqual(user_profile.weight_unit, 'lbs')
 
 # Workout
+class TestWorkout(unittest.TestCase):
 
+    def test_workout_creation(self):
+        workout = Workout("Running", 30, "medium", 300, "Morning run")
+
+        self.assertEqual(workout.workout_type, "Running")
+        self.assertEqual(workout.workout_duration, 30)
+        self.assertEqual(workout.workout_intensity, "medium")
+        self.assertEqual(workout.calories_burned, 300)
+        self.assertEqual(workout.notes, "Morning run")
 
 # Goal
+class TestGoal(unittest.TestCase):
 
+    def test_goal_creation(self):
+        goal = Goal(3, 150, 2000, "Swimming")
+
+        self.assertEqual(goal.weekly_workout_count, 3)
+        self.assertEqual(goal.weekly_duration, 150)
+        self.assertEqual(goal.weekly_calories, 2000)
+        self.assertEqual(goal.workout_type_goal, "Swimming")
 
 if __name__ == '__main__':
     unittest.main()
