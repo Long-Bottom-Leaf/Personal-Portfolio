23af9c3e1e544c24aab264bc37dc0b1bc4b6e578
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-06-26 17:33:31 -0230

    wrote two more tests for data manager

--- Python Projects/Fitness_Tracker/unit_tests/test_data_manager.py
+++ Python Projects/Fitness_Tracker/unit_tests/test_data_manager.py
@@ -20,5 +20,37 @@ class TestDataManager(unittest.TestCase):
 
             self.assertTrue(os.path.isfile(test_file_path))
 
+    def test_load_data_has_start_data(self):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            test_file_path = os.path.join(temp_dir, "fitness_data.json")
+
+            data_manager = DataManager(test_file_path)
+            data = data_manager.load_data()
+
+            self.assertEqual(data["profile"], None)
+            self.assertEqual(data["workouts"], [])
+            self.assertEqual(data["goals"], [])
+
+    def test_save_data(self):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            test_file_path = os.path.join(temp_dir, "fitness_data.json")
+
+            data_manager = DataManager(test_file_path)
+
+            test_data = {
+                "profile": {
+                    "name": "Stephen",
+                    "weight": 180,
+                    "weight_unit": "lbs"
+                },
+                "workouts": [],
+                "goals": []
+            }
+
+            data_manager.save_data(test_data)
+            loaded_data = data_manager.load_data()
+
+            self.assertEqual(loaded_data, test_data)
+
 if __name__ == '__main__':
     unittest.main()
