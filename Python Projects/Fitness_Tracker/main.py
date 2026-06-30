b50a35a788f1ebfca52f1b0c86866baedcca39d7
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-06-30 16:19:58 -0230

    added more validations in main app, working on profile/workout/goal portion

--- Python Projects/Fitness_Tracker/main.py
+++ Python Projects/Fitness_Tracker/main.py
@@ -18,6 +18,7 @@ from utils.validators import (
     validate_menu_choice
 )
 
+# Validations
 def get_valide_number(prompt):
     while True:
         value = input(prompt)
@@ -48,6 +49,65 @@ def get_valid_weight_unit():
         else:
             print("Invalid weight unit. Please enter kgs or lbs!")
 
+def get_valid_intensity():
+    while True:
+        intensity = input("Enter intensity (low/medium/hight): ")
+
+        if validate_intensity(intensity):
+            return intensity.strip().lower()
+        
+        else:
+            return("Invalid intensity. Please enter low, medium, or hight!")
+        
+def get_valid_activity():
+    while True:
+        print("\nWorkout Types:")
+
+        for code, activity in VALID_ACTIVITIES.items():
+            print(f"{code} - {activity}")
+
+        activity_code = input("Enter workout type: ")
+
+        if validate_activity(activity_code):
+            return VALID_ACTIVITIES[activity_code.strip().upper()]
+        
+        else:
+            print("Invalid workout type!")
+
+# Create profile
+def create_profile(fitness_tracker):
+    name = input("Enter your name: ")
+    weight = get_valide_number("Enter your weight: ")
+    weight_unit = get_valid_weight_unit()
+
+    profile = UserProfile(name, weight, weight_unit)
+    fitness_tracker.set_profile(profile)
+
+    print("Profile created successfully!")
+
+# Add workout
+def add_workout(fitness_tracker):
+    if fitness_tracker.profile is None:
+        print("Pease create a profile first!")
+        
+        return
+    
+    workout_type = get_valid_activity()
+    duration = get_valide_number("Enter workout duration (in minutes): ")
+    intensity = get_valid_intensity()
+    notes = input("Enter workout notes here: ")
+
+    calories_burned = calculate_calories(
+        workout_type,
+        intensity,
+        FitnessTracker.profile.user_weight,
+        duration,
+        weight_unit
+    )
+
+# Create/view goal
+
+# Menu
 def main():
     
 if __name__ == "__main__":
