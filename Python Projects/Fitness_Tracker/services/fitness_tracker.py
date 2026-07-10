c29f09d9c073d287b8fad278be4e2fe4552e0a81
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-07-10 16:42:41 -0230

    adding a few updates to workout summary printout. it was my birthday on wednesday so not a lot of coading this week, will lock in next week

--- Python Projects/Fitness_Tracker/services/fitness_tracker.py
+++ Python Projects/Fitness_Tracker/services/fitness_tracker.py
@@ -1,6 +1,7 @@
 # Temporary storage for fitness data
 
 from utils.loading_bar import loading_bar
+from services.actions import get_favorite_workout
 
 class FitnessTracker:
 
@@ -49,10 +50,19 @@ class FitnessTracker:
         total_duration = sum(workout.workout_duration for workout in self.workouts)
         total_calories = sum(workout.calories_burned for workout in self.workouts)
 
+        average_duration = round(total_duration / len(self.workouts), 1)
+        average_calories = round(total_calories / len(self.workouts), 1)
+
+        workout_type, count = self.get_favorite_workout()
+
         loading_bar("\nLoading workout summary...\n", total=20)
+        print("==== Workout Summary ====")
         print(f"Total Workouts: {len(self.workouts)}")
         print(f"Total Duration: {total_duration} minutes")
         print(f"Total Calories Burned: {total_calories} kcal")
+        print(f"Average Duration: {average_duration}")
+        print(f"Average Calories Burned: {average_calories}")
+        print(f"Favorite Workout: {workout_type} ({count} workouts)")
 
     def clear_workouts(self):
         loading_bar("\nClearing workouts...\n", total=15)
