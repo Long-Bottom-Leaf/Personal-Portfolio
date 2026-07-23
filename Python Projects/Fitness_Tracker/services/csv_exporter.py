5351bf51d9dc09f060e3221aaf63c78277d9e264
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-07-23 18:06:53 -0230

    updated csv exporter with OSError exception and a cleaner export print message

--- Python Projects/Fitness_Tracker/services/csv_exporter.py
+++ Python Projects/Fitness_Tracker/services/csv_exporter.py
@@ -4,6 +4,7 @@ import csv
 import os
 
 from utils.sorter import sort_workouts_by_date
+from utils.error_messages import FILE_SAVE_ERROR
 
 def export_workouts_to_csv(workouts, file_path="exports/workout_history.csv"):
 
@@ -11,27 +12,33 @@ def export_workouts_to_csv(workouts, file_path="exports/workout_history.csv"):
 
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
-    with open(file_path, "w", newline="") as file:
-        writer = csv.writer(file)
+    try:
+        with open(file_path, "w", newline="") as file:
+            writer = csv.writer(file)
 
-        writer.writerow([
-            "Date",
-            "Workout Type",
-            "Duration",
-            "Intensity",
-            "Calories Burned",
-            "Notes"
-        ])
-
-        for workout in sorted_workouts:
             writer.writerow([
-                workout.workout_date,
-                workout.workout_type,
-                workout.workout_duration,
-                workout.workout_intensity,
-                workout.calories_burned,
-                workout.notes
+                "Date",
+                "Workout Type",
+                "Duration",
+                "Intensity",
+                "Calories Burned",
+                "Notes"
             ])
 
-    print("\nWorkout history exported!")
-    print(f"The history was exported to {file_path}")
+            for workout in sorted_workouts:
+                writer.writerow([
+                    workout.workout_date,
+                    workout.workout_type,
+                    workout.workout_duration,
+                    workout.workout_intensity,
+                    workout.calories_burned,
+                    workout.notes
+                ])
+                
+    except OSError:
+        print(FILE_SAVE_ERROR)
+        return
+
+    print("\nWorkout history successfully exported!")
+    print(f"Location: {file_path}")
+    print(f"Total workouts exported: {len(sorted_workouts)}")
