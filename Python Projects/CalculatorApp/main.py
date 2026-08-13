1b57bfdb6b68eb8daecbd38011ce7bc0fc57df69
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-08-13 17:38:25 -0230

    basic main

--- Python Projects/CalculatorApp/main.py
+++ Python Projects/CalculatorApp/main.py
@@ -7,6 +7,9 @@ from functions.math_functions import (
     divide
 )
 from utils.validators import validate_menu_choice
+from utils.error_messages import (
+    INVALID_CHOICE
+)
 
 def display_menu():
 
@@ -16,17 +19,37 @@ def display_menu():
     print("2. Subtract")
     print("3. Multiply")
     print("4. Divide")
-    print("5. End")
+    print("5. Exit")
 
 def main():
 
     while True:
         display_menu()
 
-        choice = input("Enter choice(1/2/3/4): ")
+        choice = input("Enter choice(1/2/3/4/5): ")
 
-        if not validate_menu_choice(choice, [1, 2, 3, 4]):
-            
+        if not validate_menu_choice(choice, ["1", "2", "3", "4", "5"]):
+            print(INVALID_CHOICE)
+            continue
+
+        if choice == "5":
+            print("Goodbye!")
+            break
+
+        x = float(input("Enter first number: "))
+        y = float(input("Enter second number: "))
+
+        if choice == "1":
+            print(f"Result: {add(x, y)}")
+
+        elif choice == "2":
+            print(f"Result: {subtract(x, y)}")
+
+        elif choice == "3":
+            print(f"Result: {multiply(x, y)}")
+
+        elif choice == "4":
+            print(f"Result: {divide(x, y)}")
 
 if __name__ == "__main__":
     main()
--- Python Projects/CalculatorApp/utils/error_messages.py
+++ Python Projects/CalculatorApp/utils/error_messages.py
@@ -1,5 +1,11 @@
 # Error messages for wrong inputs and impossible math
 
+# Menu Choice
 INVALID_CHOICE = (
     "Error: Invalide choice, please enter a valid menu choice!"
+)
+
+# Number/Math errors
+DIVIDE_BY_0 = (
+    "Error: Cannot divide by zero!"
 )
