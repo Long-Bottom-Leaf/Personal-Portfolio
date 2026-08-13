def2545417e2a4ed9887e22b4dfec0427c8e5e95
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-08-13 15:37:49 -0230

    Initial commit, small calculator program, expand into more complicated math functions

---
+++ Python Projects/Calculator/main.py
@@ -0,0 +1,24 @@
+# Calculator app
+
+from functions.math_functions import (
+    add,
+    subtract,
+    multiply,
+    divide
+)
+
+def display_menu():
+
+    print("\n==== Calculator ====\n")
+
+    print("Select operation.")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. End")
+
+def main():
+
+if __name__ == "__main__":
+    main()
