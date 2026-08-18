7fc24401b2d7d3e274ae8305425d1a0c328aee52
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-08-18 16:20:40 -0230

    added unit test for advanced functions

---
+++ Python Projects/Calculator_App/unit_tests/test_advanced_functions.py
@@ -0,0 +1,33 @@
+import sys
+import os
+import unittest
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from functions.math_functions import (
+    square_root,
+    power,
+    percent,
+    aboslute_value
+)
+
+class TestAdvancedFunctions(unittest.TestCase):
+
+    def test_square_root(self):
+        self.assertEqual(square_root(25), 5)
+
+    def test_square_root_negative(self):
+        with self.assertRaises(ValueError):
+            square_root(-25)
+
+    def test_power(self):
+        self.assertEqual(power(2, 3), 8)
+
+    def test_percentage(self):
+        self.assertEqual(percent(200, 15), 30)
+
+    def test_absolute_value(self):
+        self.assertEqual(aboslute_value(-25), 25)
+
+if __name__ == "__main__":
+    unittest.main()
