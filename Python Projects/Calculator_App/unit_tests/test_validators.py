2171b87c9d410a21331f6d047463c917167d3470
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-08-25 20:21:50 -0230

    updated validators unit test, will updated further with input mock

--- Python Projects/Calculator_App/unit_tests/test_validators.py
+++ Python Projects/Calculator_App/unit_tests/test_validators.py
@@ -6,8 +6,12 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from utils.validators import (
     validate_menu_choice,
-    validate_number
+    validate_number,
+    validate_minimum_numbers,
+    get_valid_number,
+    validate_positive_number
 )
+
 
 class TestValidators(unittest.TestCase):
 
@@ -26,6 +30,25 @@ class TestValidators(unittest.TestCase):
         self.assertTrue(validate_number("-10"))
         self.assertTrue(validate_number("0"))
         self.assertFalse(validate_number("Hello world!"))
+
+    def test_validate_minimum_numbers(self):
+
+        self.assertTrue(validate_minimum_numbers([1, 2], 2))
+        self.assertTrue(validate_minimum_numbers([1, 2, 3], 2))
+        self.assertFalse(validate_minimum_numbers([1], 2))
+
+    def test_validate_positive_number(self):
+
+        self.assertTrue(validate_positive_number(10))
+        self.assertTrue(validate_positive_number(0.5))
+        self.assertFalse(validate_positive_number(0))
+        self.assertFalse(validate_positive_number(-10))
+
+    def test_get_valid_number(self):
+
+        # will updated with input mocking
+        pass
+
 
 if __name__ == "__main__":
     unittest.main()
