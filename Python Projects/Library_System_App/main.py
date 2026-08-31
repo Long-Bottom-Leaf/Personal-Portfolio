d0900ed2d85efceabe5181e530f61697508f5082
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-08-31 17:25:26 -0230

    started basic main

---
+++ Python Projects/Library_System_App/main.py
@@ -0,0 +1,18 @@
+# Library System Main
+
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+
+def display_menu():
+    print("\n==== Library Menu ====")
+
+    print("\n1. Add new book")
+    print("\n2. View books")
+    print("\n3. Remove a book")
+    print("\n4. ")
+
+    print("\n5. Exit")
+
+def main():
+    
