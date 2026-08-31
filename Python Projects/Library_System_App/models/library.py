1f2eea8dfbc69e67f45f5c5da80c2073d37f44f1
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-08-31 17:04:49 -0230

    added basic library model

---
+++ Python Projects/Library_System_App/models/library.py
@@ -0,0 +1,18 @@
+# Library model
+
+from models.book import Book
+
+class Library:
+
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def remove_book(self, book):
+        self.books.remove(book)
+
+    def view_book(self, book):
+        for book in self.books:
+            print(book)
