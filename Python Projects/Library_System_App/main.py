30a41b5f55de912179edeaf3a8e6c843b45dc570
Long-Bottom-Leaf <drcheesewizard@gmail.com>
2026-09-03 19:06:37 -0230

    building out main

--- Python Projects/Library_System_App/main.py
+++ Python Projects/Library_System_App/main.py
@@ -1,9 +1,7 @@
 # Library System Main
 
-import os
-import sys
-sys.path.insert(0, os.path.dirname(__file__))
-
+from models.book import Book
+from models.library import Library
 from utils.validators import (
     validate_menu_choice,
 )
@@ -15,13 +13,15 @@ def display_menu():
     print("\n==== Library Menu ====")
 
     print("\n1. Add new book")
-    print("\n2. View books")
+    print("\n2. View book")
     print("\n3. Remove a book")
-    print("\n4. ")
+    print("\n4. View book list")
 
     print("\n5. Exit")
 
 def main():
+
+    library = Library()
 
     while True:
         display_menu()
@@ -30,6 +30,42 @@ def main():
 
         if not validate_menu_choice(choice, ["1", "2", "3", "4", "5"]):
             print(INVALID_MENU_CHOICE)
+
+        match choice:
+            case "1":
+                print("\n==Enter book details==\n")
+                title = input("Enter title: ")
+                author = input("Enter author: ")
+                genre = input("Enter genre: ")
+                release_date = input("Enter the release date: ")
+                rating = input("Enter rating, if any: ")
+                status = input("Enter status (read/unread): ")
+
+                book = (
+                    title,
+                    author,
+                    genre,
+                    release_date,
+                    rating,
+                    status
+                )
+
+                Library.add_book(book)
+
+                print("Book added successfully!")
+
+            case "2":
+                pass
+
+            case "3":
+                pass
+
+            case "4":
+                pass
+
+            case "5":
+                print("Goodbye")
+                break
 
 if __name__ == "__main__":
     main()
